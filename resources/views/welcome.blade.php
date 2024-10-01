f747909a73c5a6cdc83d94ce403b9d56782b019b
abdurDaily <test@gmail.com>
2024-10-01 18:47:45 +0600

    1st day at office

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en" class="semi-dark">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--favicon-->
+	<link rel="icon" href="assets/images/favicon-32x32.png" type="image/png"/>
+	<!--plugins-->
+	<link href="{{ asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet"/>
+	<link href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet"/>
+	<!-- loader-->
+	<link href="{{ asset('assets/css/pace.min.css') }}" rel="stylesheet"/>
+	<script src="{{ asset('assets/js/pace.min.js') }}"></script>
+	<!-- Bootstrap CSS -->
+	<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/css/bootstrap-extended.css') }}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+	<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+	<link href="assets/css/icons.css" rel="stylesheet">
+	<!-- Theme Style CSS -->
+	<link rel="stylesheet" href="{{ asset('assets/css/dark-theme.css') }}"/>
+	<link rel="stylesheet" href="{{ asset('assets/css/semi-dark.css') }}"/>
+	<link rel="stylesheet" href="{{ asset('assets/css/header-colors.css') }}"/>
+	<title>Nikon - Bootstrap 5 Admin Template</title>
+</head>
+<body>
+
+    <div class="wrapper">
+		<div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
+			<div class="container">
+				<div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+					<div class="col mx-auto">
+						<div class="card mb-0">
+							<div class="card-body">
+								<div class="p-4">
+									<div class="mb-3 text-center">
+										<img src="assets/images/logo-icon.png" width="60" alt="" />
+									</div>
+									<div class="text-center mb-4">
+										<h5 class="">Nikon Admin</h5>
+										<p class="mb-0">Please log in to your account</p>
+									</div>
+									<div class="form-body">
+										<form class="row g-3" method="POST" action="{{ route('login') }}">
+                                            @csrf
+											<div class="col-12">
+												<label for="email" class="form-label">Email</label>
+
+                                            
+                                                <x-input-label for="email" :value="__('Email')" />
+                                                <x-text-input id="email" class="form-control" placeholder="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+
+											</div>
+											<div class="col-12">
+												<label for="password" class="form-label">Password</label>
+												{{-- <div class="input-group" id="show_hide_password"> --}}
+													
+                                                <x-input-label for="password" :value="__('Password')" />
+                                                <x-text-input id="password" class="form-control"
+                                                                type="password"
+                                                                name="password"
+                                                                placeholder="password"
+                                                                required autocomplete="current-password" />
+
+                                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+
+
+												</div>
+											</div>
+											<div class="col-md-6">
+											
+											</div>
+											<div class="col-12 mt-3">
+												<div class="d-grid">
+													<button type="submit" class="btn btn-primary">Sign in</button>
+												</div>
+											</div>
+											<div class="col-12">
+												<div class="text-center ">
+													<p class="mb-0">Don't have an account yet? <a href="{{ route('register') }}">Sign up here</a>
+													</p>
+												</div>
+											</div>
+										</form>
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--end row-->
+			</div>
+		</div>
+	</div>
+
+
+
+	<!--end switcher-->
+	<!-- Bootstrap JS -->
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script> 
+	<!--plugins-->
+	<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+	<!-- Vector map JavaScript -->
+	<script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+	<script src="{{ asset('assets/plugins/chartjs/js/chart.js') }}"></script>
+	<script src="{{ asset('assets/plugins/sparkline-charts/jquery.sparkline.min.js') }}"></script>
+	<script src="{{ asset('assets/js/index.js') }}"></script>
+	<!--app JS-->
+	<script src="{{ asset('assets/js/app.js') }}"></script>
+	<script>
+		new PerfectScrollbar('.dashboard-top-countries');
+	</script>
+
+<script>
+    $(document).ready(function () {
+        $("#show_hide_password a").on('click', function (event) {
+            event.preventDefault();
+            if ($('#show_hide_password input').attr("type") == "text") {
+                $('#show_hide_password input').attr('type', 'password');
+                $('#show_hide_password i').addClass("bx-hide");
+                $('#show_hide_password i').removeClass("bx-show");
+            } else if ($('#show_hide_password input').attr("type") == "password") {
+                $('#show_hide_password input').attr('type', 'text');
+                $('#show_hide_password i').removeClass("bx-hide");
+                $('#show_hide_password i').addClass("bx-show");
+            }
+        });
+    });
+</script>
+</body>
